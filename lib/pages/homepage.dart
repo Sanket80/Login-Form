981effb368930844a835b72faf5ee03412b15654
Sanket80 <sanketkadamsss4@gmail.com>
2024-02-16 18:45:48 +0530

    Reading Data From Firestore

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:login/read%20data/get_user_name.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,26 +11,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   final user = FirebaseAuth.instance.currentUser;
+
+  List<String> docIds = [];
+  Future getDocIds() async {
+    await FirebaseFirestore.instance
+        .collection('Users')
+        .get()
+        .then((snapshot) => snapshot.docs.forEach((document) {
+              docIds.add(document.reference.id);
+            }));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            user!.email!,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Welcome ' + user!.email!),
-            SizedBox(
-              height: 20,
-            ),
-            MaterialButton(onPressed: (){
-              FirebaseAuth.instance.signOut();
-            },
-              color: Colors.deepPurple[200],
-              child: Text('Sign Out'),
-            ),
+            Expanded(
+                child: FutureBuilder(
+                    future: getDocIds(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return ListView.builder(
+                          itemCount: docIds.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ListTile(
+                                title: GetUserName(documentId: docIds[index]),
+                                tileColor: Colors.grey[200],
+                              ),
+                            );
+                          },
+                        );
+                      } else {
+                        return CircularProgressIndicator();
+                      }
+                    })),
           ],
         ),
       ),
